78f6b5fdbdfa8187f7f023017440cbdefb0cabef
Kushang Agrawal <140241681+rogerthat-0420@users.noreply.github.com>
2025-04-25 02:54:17 +0530

    feat: update running script to run finetunes

--- run.py
+++ run.py
@@ -20,8 +20,16 @@ if __name__ == '__main__':
     
     parser.add_argument('--pred-only', dest='pred_only', action='store_true', help='only display the prediction')
     parser.add_argument('--grayscale', dest='grayscale', action='store_true', help='do not apply colorful palette')
-    
+
+    parser.add_argument('--pretrained', action='store_true', help='Use pretrained model')
+    parser.add_argument('--finetune-name', type=str, help='Name of finetuned model (required if not using pretrained)')
+    parser.add_argument('--filenames-gt', type=str)
+
     args = parser.parse_args()
+
+    if not args.pretrained and args.finetune_name is None:
+        parser.error("--finetune-name is required when --pretrained is not set.")
+
     
     DEVICE = 'cuda' if torch.cuda.is_available() else 'mps' if torch.backends.mps.is_available() else 'cpu'
     
@@ -33,28 +41,41 @@ if __name__ == '__main__':
     }
     
     depth_anything = DepthAnythingV2(**model_configs[args.encoder])
-    depth_anything.load_state_dict(torch.load(f'checkpoints/depth_anything_v2_{args.encoder}.pth', map_location='cpu'))
+    if args.pretrained :
+        print(f"Using pretrained model for interence.")
+        depth_anything.load_state_dict(torch.load(f'checkpoints/depth_anything_v2_{args.encoder}.pth', map_location='cpu'))
+    else :
+        print(f"Using finetuned model for inference")
+        depth_anything.load_state_dict(torch.load(f'saved_models/{args.finetune_name}'))
+
     depth_anything = depth_anything.to(DEVICE).eval()
     
-    if os.path.isfile(args.img_path):
-        if args.img_path.endswith('txt'):
-            with open(args.img_path, 'r') as f:
-                filenames = f.read().splitlines()
-        else:
-            filenames = [args.img_path]
-    else:
-        filenames = glob.glob(os.path.join(args.img_path, '**/*'), recursive=True)
+    # if os.path.isfile(args.img_path):
+    #     if args.img_path.endswith('txt'):
+    #         with open(args.img_path, 'r') as f:
+    #             filenames = f.read().splitlines()
+    #     else:
+    #         filenames = [args.img_path]
+    # else:
+    #     filenames = glob.glob(os.path.join(args.img_path, '**/*'), recursive=True)
+
+    filenames = [f"/scratch/soccernet/depth-basketball/Test/{args.img_path}"]
+    filenames_gt = [f"/scratch/soccernet/depth-basketball/Test/{args.filenames_gt}"]
     
     os.makedirs(args.outdir, exist_ok=True)
     
     cmap = matplotlib.colormaps.get_cmap('Spectral_r')
     
-    for k, filename in enumerate(filenames):
+    for k, (filename, filename_gt) in enumerate(zip(filenames, filenames_gt)):
         print(f'Progress {k+1}/{len(filenames)}: {filename}')
         
         raw_image = cv2.imread(filename)
+        depth_gt = cv2.imread(filename_gt)
+
+        print(depth_gt.shape)
         
         depth = depth_anything.infer_image(raw_image, args.input_size)
+
         
         depth = (depth - depth.min()) / (depth.max() - depth.min()) * 255.0
         depth = depth.astype(np.uint8)
@@ -68,6 +89,9 @@ if __name__ == '__main__':
             cv2.imwrite(os.path.join(args.outdir, os.path.splitext(os.path.basename(filename))[0] + '.png'), depth)
         else:
             split_region = np.ones((raw_image.shape[0], 50, 3), dtype=np.uint8) * 255
-            combined_result = cv2.hconcat([raw_image, split_region, depth])
+            combined_result = cv2.hconcat([raw_image, split_region, depth, split_region, depth_gt])
             
-            cv2.imwrite(os.path.join(args.outdir, os.path.splitext(os.path.basename(filename))[0] + '.png'), combined_result)
+            if args.finetune_name is not None:
+                cv2.imwrite(os.path.join(args.outdir, os.path.splitext(os.path.basename(filename))[0] + '_finetune_masked' + '.png'), combined_result)
+            else:
+                cv2.imwrite(os.path.join(args.outdir, os.path.splitext(os.path.basename(filename))[0] + '.png'), combined_result)
